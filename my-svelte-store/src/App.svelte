54c6c3a257abb42d1243ad21f7626aa08c81bbcb
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-07-25 15:43:16 +0200

    "Added categories variable and onMount function to fetch product categories from API in App.svelte"

--- my-svelte-store/src/App.svelte
+++ my-svelte-store/src/App.svelte
@@ -2,4 +2,11 @@
   import {onMount} from 'svelte';
   import ProductList from './component/ProductList.svelte';
   import Cart from './component/Cart.svelte';
+
+  let categories = [];
+
+  onMount(async() => {
+    const response = await fetch('https://fakestoreapi.com/products/categories');
+    categories = await response.json();
+  });
 </script>
