4292cb627c5f2b29b4536b8a7610f3e81be1b5c0
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-07-25 16:06:29 +0200

    "Added product list component with UI and functionality to add items to cart"

--- my-svelte-store/src/components/ProductList.svelte
+++ my-svelte-store/src/components/ProductList.svelte
@@ -9,4 +9,35 @@
     products = await response.json();
   });
 
+  function addToCart(product) {
+    cart.addItem(product);
+  }
 </script>
+
+<div class="product-list">
+  <h2>Products</h2>
+  {#each products as product}
+    <div class="product">
+      <img src={product.image} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>${product.price.toFixed(2)}</p>
+      <button on:click={() => addToCart(product)}>Add to Cart</button>
+    </div>
+  {/each}
+</div>
+
+
+<style>
+    .product-list {
+      width: 60%;
+    }
+    .product {
+      border: 1px solid #ddd;
+      padding: 10px;
+      margin-bottom: 10px;
+    }
+    img {
+      max-width: 100px;
+      max-height: 100px;
+    }
+  </style>
