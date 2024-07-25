dbe94ca4e8d63ead7de670ab74fc4edbe1e3d1bc
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-07-25 16:05:32 +0200

    "Updated ProductList.svelte to import cart from cartStore.js, added onMount function to fetch products from API, and declared products variable."

--- my-svelte-store/src/components/ProductList.svelte
+++ my-svelte-store/src/components/ProductList.svelte
@@ -1,4 +1,12 @@
 <script>
     import {onMount} from 'svelte';
-    import {cart} from './Cart.svelte'
+    import {cart} from './stores/cartStore.js'
+
+    let products = [];
+
+    onMount(async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    products = await response.json();
+  });
+
 </script>
